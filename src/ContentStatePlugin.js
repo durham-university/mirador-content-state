4c1531020805c1bf3010b8b6234a4889753dbb83
Olli Lyytinen <olli.lyytinen@durham.ac.uk>
2021-05-25 15:16:49 +0100

    Reorganise event handling

--- src/ContentStatePlugin.js
+++ src/ContentStatePlugin.js
@@ -68,50 +68,37 @@ const contentStateReducer = function(state = {}, action) {
   }
 }
 
-const contentStateSelector = createSelector(
+const windowContentStateSelector = createSelector(
   getWindow,
   state => state.contentStates,
   (window, contentStates) => contentStates[window.contentStateId]  
 );
+const contentStateSelector = createSelector(
+  (state, { contentStateId }) => contentStateId,
+  state => state.contentStates,
+  (contentStateId, contentStates) => contentStates[contentStateId]
+);
 
 const contentStateSaga = function*(){
   function* handleWindowContentState( action ){
-    const { contentState, windowId } = (() => {
+    const {contentState, contentStateId, windowId} = (() => {
       switch(action.type) {
         case ActionTypes.ADD_WINDOW:
-          return { contentState: action.window.contentState, windowId: action.window.id };
+          return {contentState: action.window.contentState, contentStateId: action.window.contentStateId, windowId: action.window.id};
         case ActionTypes.UPDATE_WINDOW:
-          return { contentState: action.payload.contentState, windowId: action.payload.id };
-        case ActionTypes.IMPORT_CONFIG:
-          return { contentState: action.config.contentState, windowId: undefined };
+          return {contentState: action.payload.contentState, contentStateId: action.payload.contentStateId, windowId: action.id};
         default:
-          return { contentState: undefined, windowId: undefined};
+          return undefined;
       }
     })();
 
-    if(!contentState) return;
+    if((!contentState && !contentStateId) || !windowId) return;
     
-    if(windowId) {
-      const record = (contentState === true) ? parseContentState(contentStateFromLocation()) : parseContentState(contentState);
-      if(!record) return;
-      yield put(updateWindow(windowId, {contentState: undefined, contentStateId: record.id}))
-      yield put(addContentState(record));
-    }
-    else {
-      yield put(addWindow({ view: "single", contentState: contentState }));
-    }
+    if(contentState) {
+      var contentStateJson = (contentState === true) ? parseContentState(contentStateFromLocation()) : parseContentState(contentState);
+      if(!contentStateJson) return;
 
-    // TODO: check if this content state already exists
-  }
-
-  yield all([
-    takeEvery(ActionTypes.IMPORT_CONFIG, handleWindowContentState ),
-    takeEvery(ActionTypes.ADD_WINDOW, handleWindowContentState ),
-    takeEvery(ActionTypes.UPDATE_WINDOW, handleWindowContentState ),
-    takeEvery('mirador/ADD_CONTENTSTATE', function*(action){
-      const payload = action.payload;
-      
-      if(payload.reference) {
+      if(contentStateJson.reference) {
         var fetchWrapper = async function(url) {
           const response = await fetch(url);
           if (response.ok) {
@@ -121,49 +108,56 @@ const contentStateSaga = function*(){
           else
             return Promise.reject({ error: response.statusText, json, response });
         }
-        var new_payload = yield call(fetchWrapper, payload.id);
-        yield put(addContentState(new_payload));
-        return;
+        contentStateJson = yield call(fetchWrapper, contentStateJson.id);
       }
+      yield put(addContentState(contentStateJson));
+      yield put(updateWindow(windowId, {contentState: undefined, contentStateId: contentStateJson.id}))
+    }
+    else if(contentStateId){
+      const contentState = yield select(contentStateSelector, { contentStateId: contentStateId })
+      if(!contentState) return;
 
-      const windows = yield select(state => state.windows);
-      for(const windowId in windows){
-        var window = windows[windowId];
-        if(window.contentStateId == payload.id) {
-          const annotationList = getContentStateAnnotation(payload.json);
-          const annotationId = annotationList["resources"][0]["@id"];
+      const annotationList = getContentStateAnnotation(contentState.json);
+      const annotationId = annotationList ? annotationList["resources"][0]["@id"] : undefined;
 
-          yield put(updateWindow(window.id, {manifestId: getContentStateManifest(payload.json), canvasId: getContentStateCanvas(payload.json), selectedAnnotationId: annotationId} ));
+      yield put(updateWindow(windowId, {manifestId: getContentStateManifest(contentState.json), canvasId: getContentStateCanvas(contentState.json), selectedAnnotationId: annotationId} ));
 
-          const companionWindows = yield select(getCompanionWindowsForContent, { windowId, content: "info" });
-          for(var companionWindow of companionWindows) {
-            yield put(updateCompanionWindow(windowId, companionWindow.id, { content: "annotations" }));
-          }
-
-
-          const box = getContentStateBox(payload.json);
-          if(box) {
-            const state = yield select();
-            console.log(state);
-
-            // TODO: zoom factor should take into account viewer geometry
-            yield put(updateViewport(windowId, {
-              x: box.x + box.w / 2.0,
-              y: box.y + box.h / 2.0,
-              zoom: 1.0 / (box.w * 3)
-            }));
-          }        
+      if(annotationId) {
+        const companionWindows = yield select(getCompanionWindowsForContent, { windowId, content: "info" });
+        for(var companionWindow of companionWindows) {
+          yield put(updateCompanionWindow(windowId, companionWindow.id, { content: "annotations" }));
         }
+
+        const box = getContentStateBox(contentState.json);
+        if(box) {
+          const state = yield select();
+          console.log(state);
+
+          // TODO: zoom factor should take into account viewer geometry
+          yield put(updateViewport(windowId, {
+            x: box.x + box.w / 2.0,
+            y: box.y + box.h / 2.0,
+            zoom: 1.0 / (box.w * 3)
+          }));
+        }        
+      }
+    }
+  }
+
+  yield all([
+    takeEvery(ActionTypes.ADD_WINDOW, handleWindowContentState ),
+    takeEvery(ActionTypes.UPDATE_WINDOW, handleWindowContentState ),
+    takeEvery(ActionTypes.IMPORT_CONFIG, function*(action){
+      if(action.config.contentState) {
+        yield put(addWindow({ view: "single", contentState: action.config.contentState}));
       }
     } ),
     takeEvery(ActionTypes.REQUEST_CANVAS_ANNOTATIONS, function*( { canvasId, windowId } ){
-
-      const contentState = yield select(contentStateSelector, { windowId })
+      const contentState = yield select(windowContentStateSelector, { windowId })
       if(!contentState) return;
       const annotation = getContentStateAnnotation(contentState.json);
       if(!annotation) return;
       yield put(receiveAnnotation(canvasId, annotation["@id"], annotation));
-
     } )
   ]);
 }
