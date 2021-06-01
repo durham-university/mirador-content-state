b84b9a44904632f68705bb581c112d2cf264bd7f
Olli Lyytinen <olli.lyytinen@durham.ac.uk>
2021-06-01 11:20:59 +0100

    Simplify content-state parsing and sagas.

--- src/ContentStatePlugin.js
+++ src/ContentStatePlugin.js
@@ -14,13 +14,7 @@ import update from 'lodash/fp/update';
 
 import fetch from 'isomorphic-unfetch';
 
-import {  contentStateFromLocation, 
-          parseContentState, 
-          getContentStateManifest, 
-          getContentStateCanvas, 
-          getContentStateAnnotation, 
-          getContentStateBox 
-      } from './contentState'; 
+import { resolveContentState } from './contentState'; 
 
 import { call, put, takeEvery, takeLatest, select, all } from 'redux-saga/effects'
 import { Select } from '@material-ui/core';
@@ -71,93 +65,60 @@ const contentStateReducer = function(state = {}, action) {
 const windowContentStateSelector = createSelector(
   getWindow,
   state => state.contentStates,
-  (window, contentStates) => contentStates[window.contentStateId]  
+  (window, contentStates) => { return {contentState: contentStates[window.contentStateId], targetIndex: window.contentStateIndex}; }
 );
-const contentStateSelector = createSelector(
-  (state, { contentStateId }) => contentStateId,
-  state => state.contentStates,
-  (contentStateId, contentStates) => contentStates[contentStateId]
+const contentStateWindowIdSelector = createSelector(
+  (state, { contentStateId, contentStateIndex}) => Object.keys(state.windows).find( id => state.windows[id].contentStateId == contentStateId && state.windows[id].contentStateIndex == contentStateIndex),
+  windowId => windowId
 );
 
 const contentStateSaga = function*(){
-  function* handleWindowContentState( action ){
-    const {contentState, contentStateId, windowId} = (() => {
-      switch(action.type) {
-        case ActionTypes.ADD_WINDOW:
-          return {contentState: action.window.contentState, contentStateId: action.window.contentStateId, windowId: action.window.id};
-        case ActionTypes.UPDATE_WINDOW:
-          return {contentState: action.payload.contentState, contentStateId: action.payload.contentStateId, windowId: action.id};
-        default:
-          return undefined;
-      }
-    })();
-
-    if((!contentState && !contentStateId) || !windowId) return;
-    
-    if(contentState) {
-      var contentStateJson = (contentState === true) ? parseContentState(contentStateFromLocation()) : parseContentState(contentState);
-      if(!contentStateJson) return;
-
-      if(contentStateJson.reference) {
-        var fetchWrapper = async function(url) {
-          const response = await fetch(url);
-          if (response.ok) {
-            const json = await response.json();
-            return Promise.resolve({ reference: false, id: url, json: json });
-          }
-          else
-            return Promise.reject({ error: response.statusText, json, response });
-        }
-        contentStateJson = yield call(fetchWrapper, contentStateJson.id);
-      }
-      yield put(addContentState(contentStateJson));
-      yield put(updateWindow(windowId, {contentState: undefined, contentStateId: contentStateJson.id}))
-    }
-    else if(contentStateId){
-      const contentState = yield select(contentStateSelector, { contentStateId: contentStateId })
-      if(!contentState) return;
-
-      const annotationList = getContentStateAnnotation(contentState.json);
-      const annotationId = annotationList ? annotationList["resources"][0]["@id"] : undefined;
-
-      yield put(updateWindow(windowId, {manifestId: getContentStateManifest(contentState.json), canvasId: getContentStateCanvas(contentState.json), selectedAnnotationId: annotationId} ));
-
-      if(annotationId) {
-        const companionWindows = yield select(getCompanionWindowsForContent, { windowId, content: "info" });
-        for(var companionWindow of companionWindows) {
-          yield put(updateCompanionWindow(windowId, companionWindow.id, { content: "annotations" }));
-        }
-
-        const box = getContentStateBox(contentState.json);
-        if(box) {
-          const state = yield select();
-          console.log(state);
-
-          // TODO: zoom factor should take into account viewer geometry
-          yield put(updateViewport(windowId, {
-            x: box.x + box.w / 2.0,
-            y: box.y + box.h / 2.0,
-            zoom: 1.0 / (box.w * 3)
-          }));
-        }        
-      }
-    }
-  }
-
   yield all([
-    takeEvery(ActionTypes.ADD_WINDOW, handleWindowContentState ),
-    takeEvery(ActionTypes.UPDATE_WINDOW, handleWindowContentState ),
     takeEvery(ActionTypes.IMPORT_CONFIG, function*(action){
       if(action.config.contentState) {
-        yield put(addWindow({ view: "single", contentState: action.config.contentState}));
+        const contentState = yield call(resolveContentState, action.config.contentState);
+        yield put(addContentState(contentState));
+
+        for(var i=0;i<contentState.targets.length;i++){
+          const target = contentState.targets[i];
+          yield put(addWindow({view: "single", manifestId: target.manifest, canvasId: target.canvas, selectedAnnotationId: target.annotation, contentStateId: contentState.id, contentStateIndex: i}) );
+          const windowId = yield select(contentStateWindowIdSelector, { contentStateId: contentState.id, contentStateIndex: i });
+
+          if(target.annotationBox) {
+            const companionWindows = yield select(getCompanionWindowsForContent, { windowId, content: "info" });
+            for(var companionWindow of companionWindows) {
+              yield put(updateCompanionWindow(windowId, companionWindow.id, { content: "annotations" }));
+            }
+          }
+        }
       }
     } ),
     takeEvery(ActionTypes.REQUEST_CANVAS_ANNOTATIONS, function*( { canvasId, windowId } ){
       const contentState = yield select(windowContentStateSelector, { windowId })
       if(!contentState) return;
-      const annotation = getContentStateAnnotation(contentState.json);
-      if(!annotation) return;
-      yield put(receiveAnnotation(canvasId, annotation["@id"], annotation));
+      const target = contentState.contentState.targets[contentState.targetIndex];
+
+      if(target.canvas == canvasId) {
+        const annotation = target.annotationList;
+        if(!annotation) return;
+        yield put(receiveAnnotation(canvasId, annotation["@id"], annotation));
+
+        const box = target.annotationBox;
+        if(box) {
+          const window = document.getElementById(windowId);
+          var zoom = 1.0;
+          if(window.clientWidth/window.clientHeight > box.w/box.h)
+            zoom = 1.0 / (box.h * 1.5 / window.clientHeight * window.clientWidth);
+          else
+            zoom = 1.0 / (box.w * 1.5);
+
+          yield put(updateViewport(windowId, {
+            x: box.x + box.w / 2.0,
+            y: box.y + box.h / 2.0,
+            zoom: zoom
+          }));
+        }
+      }
     } )
   ]);
 }
--- src/contentState.js
+++ src/contentState.js
@@ -4,14 +4,17 @@ export function contentStateFromLocation() {
     return unescape(result[1]);
 }
 
-export function parseContentState(contentState) {
-    if(!contentState) return undefined;
-    else if(typeof(contentState) == "string"){
+export async function resolveContentState(contentState) {
+    if(!contentState) return Promise.resolve(undefined);
+    if(contentState === true) contentState = contentStateFromLocation();
+    if(typeof(contentState) == "string"){
         if(contentState.startsWith("http")){
-            return {reference: true, id: contentState, json: {id: contentState}};
+            const response = await fetch(contentState);
+            if (response.ok) contentState = await response.json();
+            else return Promise.reject({ error: response.statusText, response });
         }
         else if(contentState.startsWith("{")) {
-            return parseContentState(JSON.parse(contentState));
+            contentState = JSON.parse(contentState);
         }
         else {
             var s = contentState;
@@ -22,151 +25,57 @@ export function parseContentState(contentState) {
             while((s.length)%4 != 0) s=s+"="; // add padding if it is missing, as it will be with url safe base64
             s = atob(s);
             s = decodeURIComponent(escape(s)); // this effectively goes from UTF8 to javascript string
-            return parseContentState(s);          
+            contentState = s;
         }
     }
-    else{
-        return {reference: !contentState.type, id: contentState.id, json: contentState};
-    }
+    if(!contentState) return Promise.reject({ error: "Unable to parse content state"})
+    else return Promise.resolve({id: contentState.id, targets: getContentStateTargets(contentState), json: contentState});
 }
 
-export function getContentStateManifest(json){
-    if(!json) return undefined;
+export function getContentStateTargets(json, base={}){
+    if(!json) return [base];
     switch(json.type) {
         case 'Manifest':
-            return json.id;
+            return [{...base, manifest: json.id}];
         case 'Canvas':
-            return getContentStateManifest(json.partOf || json.within);
-        case 'Annotation':
-            return getContentStateManifest(json.target);
-        default:
-            return undefined;
-    }
-}
-
-export function getContentStateCanvas(json){
-    if(!json) return undefined;
-    switch(json.type) {
-        case 'Canvas':
-            if(!json.id) return undefined;
-            var s = json.id.split("#");
-            if(s.length > 1) return s[0];
-            else return json.id;
-        case 'Annotation':
-            if(json.motivation && (json.motivation.includes("highlighting") || json.motivation.includes("contentState")) )
-                return getContentStateCanvas(json.target);
-            else return undefined;
-        default:
-            return undefined;
-    }      
-}
-
-export function getContentStateCollection(json){
-    if(!json) return undefined;
-    switch(json.type) {
-        case 'Manifest':
-            return getContentStateCollection(json.partOf || json.within);
-        case 'Canvas':
-            return getContentStateCollection(json.partOf || json.within);
-        case 'Annotation':
-            if(json.motivation && (json.motivation.includes("highlighting") || json.motivation.includes("contentState")) )
-                return getContentStateCollection(json.target);
-            else return undefined;
-        default:
-            return undefined;
-    }
-}
-
-export function getContentStateBox(json){
-    if(!json) return undefined;
-    switch(json.type) {
-        case 'Canvas':
-            if(!json.id) return undefined;
-            var s = json.id.split("#");
-            if(s.length <= 0) return undefined;
-            var m = s[1].match(/xywh=(\d+),(\d+),(\d+),(\d+)/);
-            if(!m) return undefined;
-            return {x: parseInt(m[1]), y: parseInt(m[2]), w: parseInt(m[3]), h: parseInt(m[4])};
-        case 'Annotation':
-            if(json.motivation && (json.motivation.includes("highlighting") || json.motivation.includes("contentState")) ) {
-                return getContentStateBox(json.target);
-            }
-            else return undefined;
-        default:
-            return undefined;
-    }      
-}
-
-export function getContentStateAnnotation(json){
-    if(!json) return undefined;
-    switch(json.type) {
-        case 'Canvas':
-            if(!json.id) return undefined;
+            if(!json.id) return [];
             var s = json.id.split("#");
             if(s.length > 1) {
+                base = {...base, canvas: s[0] };
                 var m = s[1].match(/xywh=(\d+),(\d+),(\d+),(\d+)/);
-                if(!m) return undefined
-                var path = "M" + m[1] + "," + m[2] + "l" + m[3] + ",0l0," + m[4] + "l-" + m[3] + ",0z";
-                return {
-                    "@context": "http://www.shared-canvas.org/ns/context.json",
-                    "@id": s[0] + "_contentState_annotationList",
-                    "@type": "sc:AnnotationList",
-                    "resources": [{
-                        "@id": s[0] + "_contentState_" + m[1] + "," + m[2] + "," + m[3] + "," + m[4],
-                        "@type": "oa:Annotation",
-                        "motivation": "oa:commenting",
-                        "on": json.id,
-                        "resource": {
-                            "@type": "cnt:ContentAsText",
-                            "chars": "Link target",
-                            "format": "text/plain"
-                        }
-                    }]
-                };
-                /*return { 
-                    "@context": "http://iiif.io/api/presentation/3/context.json",
-                    "@id": s[0] + "_contentState_annotationPage",
-                    "type": "AnnotationPage",
-                    "items": [{
-                        "@context": "http://iiif.io/api/presentation/2/context.json",
-                        "@id": s[0] + "_contentState_" + m[1] + "," + m[2] + "," + m[3] + "," + m[4],
-                        "@type": "oa:Annotation",
-                        "label": "",
-                        "motivation": "sc:painting",
-                        "on": [{
-                            "@type": "oa:SpecificResource",
-                            "full": s[0],
-                            "selector": {
-                                "@type": "oa:Choice",
-                                "default": {
-                                    "@type": "oa:FragmentSelector",
-                                    "value": s[1]
-                                },
-                                "item": {
-                                    "@type": "oa:SvgSelector",
-                                    "value": "<svg xmlns='http://www.w3.org/2000/svg'><path xmlns=\"http://www.w3.org/2000/svg\" d=\"" + path +"\" data-paper-data=\"{\u0026quot;strokeWidth\u0026quot;:4,\u0026quot;rotation\u0026quot;:0,\u0026quot;deleteIcon\u0026quot;:null,\u0026quot;rotationIcon\u0026quot;:null,\u0026quot;group\u0026quot;:null,\u0026quot;editable\u0026quot;:false,\u0026quot;annotation\u0026quot;:null}\" id=\"rectangle_content_state\" fill-opacity=\"0\" stroke=\"#ff3d16\" stroke-width=\"4\" stroke-linecap=\"butt\" stroke-linejoin=\"miter\" stroke-dasharray=\"20 20\"/></svg>"
-                                }
+                if(m) {
+                    var annotationId = s[0] + "_contentState_" + m[1] + "," + m[2] + "," + m[3] + "," + m[4];
+                    base = {...base, annotationList: {
+                        "@context": "http://www.shared-canvas.org/ns/context.json",
+                        "@id": s[0] + "_contentState_annotationList",
+                        "@type": "sc:AnnotationList",
+                        "resources": [{
+                            "@id": annotationId,
+                            "@type": "oa:Annotation",
+                            "motivation": "oa:commenting",
+                            "on": json.id,
+                            "resource": {
+                                "@type": "cnt:ContentAsText",
+                                "chars": "Link target",
+                                "format": "text/plain"
                             }
                         }]
-                    }]
-                };*/
+                    }, annotation: annotationId, annotationBox: {x: parseInt(m[1]), y: parseInt(m[2]), w: parseInt(m[3]), h: parseInt(m[4])} };
+                } 
             }
-            else return undefined;
+            else base = {...base, canvas: json.id};
+
+            return getContentStateTargets(json.partOf || json.within, base)
         case 'Annotation':
-//            return json;
-            if(json.motivation && (json.motivation.includes("highlighting") || json.motivation.includes("contentState")) ) {
-                var annotation = getContentStateAnnotation(json.target);
-                if(annotation && json.resource && json.resource.chars) {
-                    return {...annotation, "resources": [{...(annotation.resources[0]), "resource": {
-                        "@type": "cnt:ContentAsText",
-                        "chars": json.resource.chars,
-                        "format": "text/plain"
-                      }
-                    }]};
-                }
-                else return annotation;
+            if(json.motivation && (json.motivation.includes("highlighting") || json.motivation.includes("contentState")) ){
+                if(json.resource && json.resource.chars) base = {...base, annotationResource: json.resource};
+
+                if(json.target instanceof Array) 
+                    return json.target.flatMap(x => getContentStateTargets(x, base));
+                else
+                    return getContentStateTargets(json.target, base);
             }
         default:
-            return undefined;
-    }  
+            return [base];
+    }
 }
